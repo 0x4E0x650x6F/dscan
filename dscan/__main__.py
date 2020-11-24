13dcddb3917f1ccd284eb9a76fa6cc1711801283
0x4E0x650x6F <tiago.alexand@gmail.com>
2020-11-24 12:33:30 +0000

    main done

--- dscan/__main__.py
+++ dscan/__main__.py
@@ -7,7 +7,7 @@ import os
 import threading
 from configparser import ConfigParser, ExtendedInterpolation
 from datetime import datetime
-
+import shutil
 from dscan.client import Agent
 from dscan.models.scanner import Config
 from dscan.server import DScanServer
@@ -17,15 +17,22 @@ from dscan.out import ContextDisplay
 
 FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
 
+
 def create_config(options):
     cfg = ConfigParser(interpolation=ExtendedInterpolation())
-    data = open(os.path.join(dataPath, 'dscan.conf'))
+    conf_path = os.path.join(dataPath, 'dscan.conf')
+    if options.config:
+        conf_path = options.config
+    data = open(conf_path)
     cfg.read_file(data)
     data.close()
-    return Config(cfg, options)
+    config = Config(cfg, options)
+    config.target_optimization(options.targets)
+    return config
 
 
 def create_server(options):
+
     settings = create_config(options)
     server = DScanServer((settings.host, settings.port),
                          AgentHandler, options=settings)
@@ -33,51 +40,66 @@ def create_server(options):
     server_thread = threading.Thread(target=server.serve_forever)
     # Exit the server thread when the main thread terminates
     server_thread.daemon = True
-    server_thread.start()
-    logging.info(f"Server loop running in thread:{server_thread.name}")
-    out = ContextDisplay(server.ctx)
-    out.show()
-    return server
+    try:
+        server_thread.start()
+        logging.info(f"Server loop running in thread:{server_thread.name}")
+        out = ContextDisplay(server.ctx)
+        out.show()
+        server_thread.join()
+    except (KeyboardInterrupt, Exception) as ex:
+        logging.info("Forced shutdown was requested!")
+        logging.info(f"{ex}")
+        if server:
+            server.shutdown()
 
 
 def create_agent(options):
     settings = create_config(options)
     agent = Agent(settings)
     agent_thread = threading.Thread(target=agent.start)
-    agent_thread.start()
-    return agent
-
-
-def main():
-    worker = None
     try:
-        if args.cmd == "agent":
-            worker = create_agent(args)
-        else:
-            worker = create_server(args)
+        agent_thread.start()
+        agent_thread.join()
     except (KeyboardInterrupt, Exception) as ex:
         logging.info("Forced shutdown was requested!")
         logging.info(f"{ex}")
-        if worker:
-            worker.shutdown()
+        if agent_thread:
+            agent.shutdown()
+
+
+def setup_config(options):
+    os.makedirs(options.name, exist_ok=True)
+    shutil.copy(os.path.join(dataPath, "dscan.conf"),
+                os.path.join(options.name, "dscan.conf"))
+
+
+def main():
+    if args.cmd == "agent":
+        create_agent(args)
+    elif args.cmd == "srv":
+        create_server(args)
+    else:
+        setup_config(args)
 
 
 if __name__ == "__main__":
     now = datetime.now().strftime("%b-%d-%Y-%H-%M")
 
     logging.basicConfig(filename=f"drecon-{now}.log", format=FORMAT,
-                        level=logging.INFO)
+                        level=logging.DEBUG)
 
     parser = argparse.ArgumentParser(prog='Distributed scanner')
     parser.add_argument('--name', type=str, required=True)
+    parser.add_argument('--config', required=True)
     subparsers = parser.add_subparsers(dest='cmd')
     subparsers.required = True
     parser_server = subparsers.add_parser('srv')
     parser_server.add_argument('-b', default='0.0.0.0')
-    parser_server.add_argument('-p', type=str, default='2040')
+    parser_server.add_argument('-p', type=int, default=2040)
     parser_server.add_argument('targets', type=argparse.FileType('rt'))
     parser_agent = subparsers.add_parser('agent')
-    parser_agent.add_argument('-s', default='0.0.0.0', required=True)
+    parser_agent.add_argument('-s', required=True)
     parser_agent.add_argument('-p', type=str, default='2040')
+    parser_agent = subparsers.add_parser('config')
     args = parser.parse_args()
     main()
